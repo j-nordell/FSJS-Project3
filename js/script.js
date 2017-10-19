64c6d141aa9dc85fe4c449f92d7601b743de3733
Jennifer Nordell <jennifer.nordell@outlook.com>
2017-10-19 18:04:40 +0200

    field validation complete

--- js/script.js
+++ js/script.js
@@ -166,7 +166,58 @@ function checkAvailability(activityIndex) {
 }
 
 function createWarnings() {
-    
+    let nameWarning = document.createElement("span");
+    nameWarning.setAttribute("id", "name-warning");
+    let nameWarningText = document.createTextNode(`Can only contain letters and spaces`);
+    let nameInput = document.getElementById("name");
+    nameWarning.appendChild(nameWarningText);
+    nameInput.insertAdjacentElement("afterend", nameWarning);
+
+    let emailWarning = document.createElement("span");
+    emailWarning.setAttribute("id", "email-warning");
+    let emailWarningText = document.createTextNode(`Valid email address required.`);
+    let emailInput = document.getElementById("mail");
+    emailWarning.appendChild(emailWarningText);
+    emailInput.insertAdjacentElement("afterend", emailWarning);
+
+    let cardNumberWarning = document.createElement("span");
+    cardNumberWarning.setAttribute("id", "card-number-warning");
+    let cardWarningText = document.createTextNode(`Must be 16 digits`);
+    let cardNumberInput = document.getElementById("cc-num");
+    cardNumberWarning.appendChild(cardWarningText);
+    cardNumberInput.insertAdjacentElement("afterend", cardNumberWarning);
+
+    let zipCodeWarning = document.createElement("span");
+    zipCodeWarning.setAttribute("id", "zip-warning");
+    let zipCodeWarningText = document.createTextNode(`Must be 5 digits`);
+    let zipCodeInput = document.getElementById("zip");
+    zipCodeWarning.appendChild(zipCodeWarningText);
+    zipCodeInput.insertAdjacentElement("afterend", zipCodeWarning);
+
+    let cvvWarning = document.createElement("span");
+    cvvWarning.setAttribute("id", "cvv-warning");
+    let cvvWarningText = document.createTextNode(`Must be 3 digits`);
+    let cvvInput = document.getElementById("cvv");
+    cvvWarning.appendChild(cvvWarningText);
+    cvvInput.insertAdjacentElement("afterend", cvvWarning);
+
+    hideAllWarnings();
+}
+
+function hideAllWarnings() {
+    document.getElementById("name-warning").style.display = "none";
+    document.getElementById("email-warning").style.display = "none";
+    document.getElementById("card-number-warning").style.display ="none";
+    document.getElementById("zip-warning").style.display = "none";
+    document.getElementById("cvv-warning").style.display = "none";
+}
+
+function toggleWarning(show, warning) {
+    if(show) {
+        warning.style.display = "inherit";
+    } else {
+        warning.style.display = "none";
+    }
 }
 
 
@@ -212,7 +263,43 @@ paymentSelect .addEventListener("change", function() {
     }
 });
 
+function createListener(validator) {
+    return e => {
+        const text = e.target.value;
+        const valid = validator(text);
+        const showWarning = text !== "" && !valid;
+        const warning = e.target.nextElementSibling;
+        toggleWarning(showWarning, warning);
+    }
+}
+
+document.getElementById("name").addEventListener("input", createListener(isValidName));
+document.getElementById("mail").addEventListener("input", createListener(isValidEmail));
+document.getElementById("cc-num").addEventListener("input", createListener(isValidCardNumber));
+document.getElementById("zip").addEventListener("input", createListener(isValidZipcode));
+document.getElementById("cvv").addEventListener("input", createListener(isValidCVV));
+
 
 // ================
-// Validations
+// Validators
 // ================
+
+function isValidName(name) {
+    return  /^\D+$/.test(name);
+}
+
+function isValidEmail(email) {
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+}
+
+function isValidCardNumber(cardNumber) {
+    return /^\D*\d{4}\D*\D*\d{4}\D*\d{4}\D*\d{4}D*$/.test(cardNumber);
+}
+
+function isValidZipcode(zipcode) {
+    return /^\d{5}$/.test(zipcode);
+}
+
+function isValidCVV(cvv) {
+    return /^\d{3}$/.test(cvv);
+} 
